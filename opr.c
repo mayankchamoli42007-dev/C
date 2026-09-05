72075b75631109422753c1ea7149ecf6174e2f3b
mayankchamoli42007-dev <mayankchamoli42007@gmail.com>
2026-09-06 02:41:32 +0530

    program to input two numbers and display their difference, product, and quotient.

---
+++ opr.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n1;
+    printf("enter non zero first num");
+    scanf("%d",&n1);
+    int n2;
+    printf("enter non zero second number");
+    scanf("%d",&n2);
+    printf("difference of two num is :%d \n",n1-n2);
+    printf("product of two num is:%d \n",n1*n2);
+    printf("quotient of number is :%d \n",n1/n2);
+    return 0;
+
+
+}
