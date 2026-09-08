6661581d9c761da5df3bcaa05d0d4c987488c975
mayankchamoli42007-dev <mayankchamoli42007@gmail.com>
2026-09-08 20:54:34 +0530

    Write a program to calculate the area and circumference of a circle given its radius.

---
+++ day4code1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int r;
+    printf("enter radius of circle");
+    scanf("%d",&r);
+    printf("area of circle is:%f \n",3.14*r*r);
+    printf("circumference of circle is :%f \n",2*3.14*r);
+    return 0;
+}
