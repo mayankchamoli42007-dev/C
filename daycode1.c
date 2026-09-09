bd1237ffdfbf4aef16a244ff3c7a745474db2dfe
mayankchamoli42007-dev <mayankchamoli42007@gmail.com>
2026-09-09 22:41:22 +0530

    program to swap two numbers using a third variable.

---
+++ daycode1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter two number");
+    scanf("%d %d",&a,&b);
+    printf("before swapping value are:%d %d",a,b);
+    int c=a;
+        a=b;
+        b=c;
+    printf("after swapping value are :%d %d \n",a,b);
+    return 0;
+}
